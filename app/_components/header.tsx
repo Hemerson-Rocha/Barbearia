66144ee015947086ea9dc8c5cb3992391fceddc1
Hemerson-Rocha <hemerson.me@gmail.com>
2024-02-08 16:24:47 -0300

    feat: add AuthProvider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,18 @@
+"use client"
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+// import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+    // const {data} = useSession();
+
+    // const handleLoginClick = async () => {
+    //     await signIn()
+    // }
+
     return ( 
         <Card>
             <CardContent className="p-5 justify-between flex flex-row items-center">
@@ -11,6 +20,15 @@ const Header = () => {
                 <Button variant="outline" size='icon' className="h-8 w-8">
                     <MenuIcon size={18}/>
                 </Button>
+                
+                {/* {data?.user ? (
+                        <div className="">
+                            <Button onClick={() => signOut()}>Logout</Button>
+                            <h2>{data.user.name}</h2>
+                        </div>
+                    ) : (
+                        <Button onClick={handleLoginClick}>login</Button>
+                    )} */}
             </CardContent>
         </Card>
      );
