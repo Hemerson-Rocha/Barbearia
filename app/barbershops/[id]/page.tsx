11b7d879d903656837df43f9a6f164c40d2e6fad
Hemerson-Rocha <hemerson.me@gmail.com>
2024-02-13 17:44:39 -0300

    feat: add initial service booking

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 
 interface BarbershopDetailsPageProps {
@@ -10,6 +12,8 @@ interface BarbershopDetailsPageProps {
 }
 
 const BarbershopDatailsPage = async ({params}: BarbershopDetailsPageProps) => {
+    const session = await getServerSession(authOptions)
+
     if(!params.id) {
         // TODO: redirecionar para home page
         return null
@@ -39,7 +43,9 @@ const BarbershopDatailsPage = async ({params}: BarbershopDetailsPageProps) => {
                 {barbershop.services.map(service =>
                     <ServiceItem
                         key={service.id}
-                        service={service} />
+                        barbershop={barbershop}
+                        service={service}
+                        isAuthenticated={!!session?.user} />
                 )}
             </div>
         </div>
